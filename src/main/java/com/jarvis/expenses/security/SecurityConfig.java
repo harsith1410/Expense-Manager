d9b8573c789bda7b2e0fa1a72f42a7e7072ff581
harsith1410 <harsith1410@gmail.com>
2025-09-08 23:13:10 +0530

    Login using the MySQL backend DB

--- src/main/java/com/jarvis/expenses/security/SecurityConfig.java
+++ src/main/java/com/jarvis/expenses/security/SecurityConfig.java
@@ -2,28 +2,38 @@ package com.jarvis.expenses.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
 
+//    @Bean
+//    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+//        UserDetails user = User.builder()
+//                .username("USER")
+//                .password("{noop}password")
+//                .build();
+//
+//        UserDetails admin = User.builder()
+//                .username("ADMIN")
+//                .password("{noop}test123#")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(user,admin);
+//    }
+
+
     @Bean
-    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
-        UserDetails user = User.builder()
-                .username("USER")
-                .password("{noop}password")
-                .build();
-
-        UserDetails admin = User.builder()
-                .username("ADMIN")
-                .password("{noop}test123#")
-                .build();
-
-        return new InMemoryUserDetailsManager(user,admin);
+    public JdbcUserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
@@ -46,6 +56,9 @@ public class SecurityConfig {
                 .exceptionHandling(configurer ->
                         configurer.accessDeniedPage("/"))
         ;
+
+        http.httpBasic(Customizer.withDefaults());
+
 
         return http.build();
     }
